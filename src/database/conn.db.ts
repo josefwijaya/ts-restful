781cf296bce636f2b9930b8f220843f853739998
Josef <josefwijaya@outlook.com>
2019-04-22 13:39:44 +0800

    update db - importing model files

--- src/database/conn.db.ts
+++ src/database/conn.db.ts
@@ -1,9 +1,12 @@
 import fs from 'fs';
-import { Sequelize } from 'sequelize';
+import ar from 'path';
+import { Sequelize, Model } from 'sequelize';
 // import { HasManyGetAssociationsMixin, HasManyAddAssociationMixin, HasManyHasAssociationMixin, Association, HasManyCountAssociationsMixin, HasManyCreateAssociationMixin } from '../../lib/associations';
 
 export class Connection {
-  private sequelize:any;
+  private sequelize:Sequelize;
+  models: any;
+  // private models:{ User:Model };
 
   constructor (uri:string, logging:boolean) {
     this.sequelize = new Sequelize(uri, { logging });
@@ -15,19 +18,20 @@ export class Connection {
 
   /** load models */
   async load (path:string, pattern:string) {
-    // fs.readdir(directoryPath, function (err, files) {
-    //   //handling error
-    //   if (err) {
-    //       return console.log('Unable to scan directory: ' + err);
-    //   }
-    //   //listing all files using forEach
-    //   files.forEach(function (file) {
-    //       // Do whatever you want to do with the file
-    //       console.log(file);
-    //   });
+    const modelPaths = this.getModels(path, pattern);
+    const models:any = [];
+
+    modelPaths.forEach(e => {
+      const m = require(ar.resolve(e));
+      models.push(m);
+    });
+
+    // const promises:any[] = [];
+    // modelPaths.forEach(e => {
+    //   promises.push(import(ar.resolve(e)));
     // });
-    const models = this.getModels(path, pattern);
-    console.log('res', models)
+    // const models = await Promise.all(promises);
+    console.log('1', models);
   }
 
   /** sync modules models with the database */
@@ -48,12 +52,10 @@ export class Connection {
     let models:string[] = [];
     files.forEach(e => {
       if (e.split('.').length === 1) {
-        console.log(`1 ${path}/${e}`);
+        // recursive get all model files
         models = models.concat(this.getModels(`${path}/${e}`, pattern));
-        console.log('2 ', models);
       }
       if (e.match(pattern)) {
-        console.log(true, path + e);
         models.push(path + '/' + e); // ./src/modules + / + user.model.ts
       }
     });
--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,16 +1,45 @@
-import { Model, DataTypes } from 'sequelize';
+// import { Model, DataTypes } from 'sequelize';
 
-export class User extends Model {}
+// export class User extends Model {
+//   static getAttr () {
+//     return
+//   }
+// }
 
-export const init = {
+// export const init = {
+//   attributes: {
+//     id: {
+//       type: new DataTypes.INTEGER.UNSIGNED(),
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+//     name: {
+//       type: new DataTypes.STRING(128),
+//       allowNull: false,
+//     },
+//   },
+//   options: {
+//     tableName: 'Users',
+//     modelName: 'User',
+//     // sequelize: sequelize, // this bit is important
+//   }
+// };
+
+// export const association = function (models) {
+
+// }
+
+const Sequelize = require('sequelize');
+
+exports.init = {
   attributes: {
     id: {
-      type: new DataTypes.INTEGER.UNSIGNED(),
+      type: Sequelize.STRING,
       autoIncrement: true,
       primaryKey: true,
     },
     name: {
-      type: new DataTypes.STRING(128),
+      type: Sequelize.STRING,
       allowNull: false,
     },
   },
@@ -19,4 +48,4 @@ export const init = {
     modelName: 'User',
     // sequelize: sequelize, // this bit is important
   }
-}
+};
